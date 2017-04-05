c2774156ac06727939483b0a0f4ca835f84d7d00
budihuang <huangpeizhong@huangpeizhongdeMacBook-Pro.local>
2017-04-05 21:37:59 +0800

    wrong branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :groups do
     member do
       post :join
@@ -7,5 +8,9 @@ Rails.application.routes.draw do
     end
     resources :posts
   end
+namespace :account do
+  resoures :groups
+end
+
   root 'groups#index'
 end
