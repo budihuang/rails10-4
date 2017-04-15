b49ed46523db9fce9b874f1798c5f7817590f2cf
budihuang <huangpeizhong@pp.local>
2017-04-15 16:11:40 +0800

    add post-edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,39 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  def edit#编辑
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @group.user  #!=的意思是不等于
+        redirect_to root_path, alert: "You have no permission."
+    end
+  end
+  def update#修改新的内容
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+   if current_user != @group.user
+    redirect_to root_path,alert: "you are no permission"
+   end
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy#删除
+
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @group.user
+       redirect_to root_path, alert: "You have no permission."
+     end
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path
+
+  end
+
 
 
   private
