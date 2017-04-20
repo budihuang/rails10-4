4e268e9da397103ff75bfe16535a894fb79ef2a4
budihuang <huangpeizhong@pp.local>
2017-04-20 19:17:13 +0800

    implement  post edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,47 +1,48 @@
-
 class PostsController < ApplicationController
-
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :find_group
 
   def new
-    @group = Group.find(params[:group_id])
-    @post = Post.new
+    @post = @group.posts.new
+  end
+
+  def edit
+    @post = @group.posts.find(params[:id])
   end
 
   def create
-    @group = Group.find(params[:group_id])
-    @post = Post.new(post_params)
-    @post.group = @group
-    @post.user = current_user
+    @post = @group.posts.build(post_params)
 
     if @post.save
-      redirect_to group_path(@group)
+      redirect_to group_path(@group), notice: "新增文章成功！"
     else
       render :new
     end
   end
-  def edit
-     @group = Group.find(params[:group_id])
-     @post = @group.posts.find(params[:id])
+
+  def update
+    @post = @group.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to group_path(@group), notice: "文章修改成功！"
+    else
+      render :edit
     end
+  end
 
+  def destroy
+    @post = @group.posts.find(params[:id])
 
-    def update
-     @group = Group.find(params[:group_id])
-     @post = @group.posts.find(params[:id])
-
-     if @post.update(post_params)
-       redirect_to group_path(@group), notice: "文章修改成功！"
-     else
-       render :edit
-     end
-    end
-
+    @post.destroy
+    redirect_to group_path(@group), alert: "文章已刪除"
+  end
 
   private
+
+  def find_group
+    @group = Group.find(params[:group_id])
+  end
 
   def post_params
     params.require(:post).permit(:content)
   end
-
 end
